7d3c9535889fc0d15f019ef4edc36e487a02c141
unknown <seby_schlachter@yahoo.com>
2021-12-01 14:18:53 +0200

    Tema 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { TodoComponent } from './components/dashboard/todo/todo.component';
+import { TodoComponent } from './components/todo/todo.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FavoriteComponent } from './components/favorite/favorite.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { CoinComponent } from './components/coin/coin.component';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    TodoComponent
+    TodoComponent,
+    DashboardComponent,
+    FavoriteComponent,
+    FavoritesComponent,
+    CoinComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
